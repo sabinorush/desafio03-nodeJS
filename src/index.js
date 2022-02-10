2ae52bb429844f5705fef07815ed489f5d86e64d
sabinorush <gu.sabino@hotmail.com>
2022-02-10 11:14:36 -0300

    Solução desafio-03 :tada:

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 const express = require("express");
-
 const { v4: uuid } = require("uuid");
 
 const app = express();
-
 app.use(express.json());
 
 const repositories = [];
-
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
-});
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
@@ -23,20 +17,33 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.get("/repositories", (request, response) => {
+
+  return response.json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const newRepository = {
+    title,
+    url,
+    techs,
+  }
+
+  const repository = { ...repositories[repositoryIndex], ...newRepository };
 
   repositories[repositoryIndex] = repository;
 
@@ -48,7 +55,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -68,7 +75,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
